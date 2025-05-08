b1ff6dd7af03e76f2acceb2b4d697f49a03c6ddf
Gregory Sprenger <gregory.sprenger@student.uibk.ac.at>
2025-05-08 16:04:05 +0200

    properties

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
 // Innsbruck
 let ibk = {
-    lat: 47.267222,
-    lng: 11.392778,
-    zoom: 11,
+  lat: 47.267222,
+  lng: 11.392778,
+  zoom: 11,
 };
 
 // Karte initialisieren
@@ -12,50 +12,57 @@ let map = L.map("map").setView([ibk.lat, ibk.lng], ibk.zoom);
 
 // thematische Layer
 let overlays = {
-    stations: L.featureGroup().addTo(map),
+  stations: L.featureGroup().addTo(map),
 }
 
 // Layer control
 L.control.layers({
-    "Relief avalanche.report": L.tileLayer(
-        "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
-        attribution: `© <a href="https://sonny.4lima.de">Sonny</a>, <a href="https://www.eea.europa.eu/en/datahub/datahubitem-view/d08852bc-7b5f-4835-a776-08362e2fbf4b">EU-DEM</a>, <a href="https://lawinen.report/">avalanche.report</a>, all licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>`
-    }).addTo(map),
-    "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
-    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
-    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+  "Relief avalanche.report": L.tileLayer(
+    "https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
+    attribution: `© <a href="https://sonny.4lima.de">Sonny</a>, <a href="https://www.eea.europa.eu/en/datahub/datahubitem-view/d08852bc-7b5f-4835-a776-08362e2fbf4b">EU-DEM</a>, <a href="https://lawinen.report/">avalanche.report</a>, all licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>`
+  }).addTo(map),
+  "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+  "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
+  "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
 }, {
-    "Wetterstationen": overlays.stations,
+  "Wetterstationen": overlays.stations,
 }).addTo(map);
 
 // Maßstab
 L.control.scale({
-    imperial: false,
+  imperial: false,
 }).addTo(map);
 
 // Wetterstationen
 async function loadStations(url) {
-    let response = await fetch(url);
-    let jsondata = await response.json();
+  let response = await fetch(url);
+  let jsondata = await response.json();
 
-    // Wetterstationen mit Icons und Popups
-    L.geoJSON(jsondata, {
-        pointToLayer: function (feature, latlng) {
-          return L.marker(latlng, {
-            icon: L.icon({
-              iconUrl: "icons/wifi.png",
-              iconSize: [32, 37],
-              iconAnchor: [16, 37],
-              popupAnchor: [0, -37]
-            })
-          });
-        },
-      
-        onEachFeature: function (feature, layer) {
-          layer.bindPopup(`
+  // Wetterstationen mit Icons und Popups
+  L.geoJSON(jsondata, {
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, {
+        icon: L.icon({
+          iconUrl: "icons/wifi.png",
+          iconSize: [32, 37],
+          iconAnchor: [16, 37],
+          popupAnchor: [0, -37]
+        })
+      });
+    },
+
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(`
             <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
-          `);
-        }
-      }).addTo(overlays.stations);
+            <ul>
+            <li>Luftemperatur (c) ${feature.properties.LT || "-"}</li>
+            <li>Relative Luftfeuchte (%) ${feature.properties.RH || "-"}</li>
+            <li>Windgeschwindigkeit (km/h) ${feature.properties.WG || "-"}</li>
+            <li>Schneehöhe (cm) ${feature.properties.HS || "-"}</li>
+            </ul>
+            <span></span> 
+            `);
+    }
+  }).addTo(overlays.stations);
 }
 loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
